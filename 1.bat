b1fb82dd9ca0d11abbe0fed4bd41460e6b9853b3
Caio-Simão <kylesimon.workmail@gmail.com>
2025-10-24 12:35:48 -0300

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,44 @@
+:: Author: tsgrgo
+:: Completely disable Windows Update
+:: PsExec is required to get system privileges - it should be in this directory
+
+if not "%1"=="admin" (powershell start -verb runas '%0' admin & exit /b)
+if not "%2"=="system" (powershell . '%~dp0\PsExec.exe' /accepteula -i -s -d '%0' admin system & exit /b)
+
+:: Disable update related services
+for %%i in (wuauserv, UsoSvc, uhssvc, WaaSMedicSvc) do (
+	:retry
+	sc config %%i start= disabled
+	sc failure %%i reset= 0 actions= ""
+	net stop %%i
+	(sc query %%i | find "RUNNING" || sc query %%i | find "STOP_PENDING") && timeout /t 2 && goto retry
+)
+
+:: Brute force rename services
+for %%i in (WaaSMedicSvc, wuaueng) do (
+	takeown /f C:\Windows\System32\%%i.dll && icacls C:\Windows\System32\%%i.dll /grant *S-1-1-0:F
+	rename C:\Windows\System32\%%i.dll %%i_BAK.dll
+	icacls C:\Windows\System32\%%i_BAK.dll /setowner "NT SERVICE\TrustedInstaller" && icacls C:\Windows\System32\%%i_BAK.dll /remove *S-1-1-0
+)
+
+:: Update registry
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 4 /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v FailureActions /t REG_BINARY /d 000000000000000000000000030000001400000000000000c0d4010000000000e09304000000000000000000 /f
+reg add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
+
+:: Delete downloaded update files
+erase /f /s /q c:\windows\softwaredistribution\*.* && rmdir /s /q c:\windows\softwaredistribution
+
+:: Disable all update related scheduled tasks
+powershell -NoProfile -Command $paths = @( ^
+'\Microsoft\Windows\InstallService*', ^
+'\Microsoft\Windows\UpdateOrchestrator*', ^
+'\Microsoft\Windows\UpdateAssistant*', ^
+'\Microsoft\Windows\WaaSMedic*', ^
+'\Microsoft\Windows\WindowsUpdate*', ^
+'\Microsoft\WindowsUpdate*' ^
+); ^
+foreach ($path in $paths) { Get-ScheduledTask -TaskPath $path ^| Disable-ScheduledTask -ErrorAction SilentlyContinue }
+
+echo Finished
+pause
---
+++ 3.bat
@@ -0,0 +1,16 @@
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Personalization /v NoLockScreen /t REG_DWORD /d 1 /f
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TimeBrokerSvc /v Start /t REG_DWORD /d 4 /f
+reg add HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\TokenBroker /v Start /t REG_DWORD /d 4 /f
+sc config "edgeupdate" start= disabled
+sc config "edgeupdatem" start= disabled
+sc config "RpcSs" start= disabled
+sc config "wuauserv" start= disabled
+sc config "TapiSrv" start= disabled
+sc config "TimeBrokerSvc" start= disabled
+sc config "MicrosoftEdgeElevationService" start= disabled
+sc config "AarSvc_3e1e2" start= disabled
+sc config "ClickToRunSvc" start= disabled
+sc config "PhoneSvc" start= disabled
+sc config "TapiSrv" start= disabled
+sc config "TapiSrv" start= disabled
+sc config "TapiSrv" start= disabled
